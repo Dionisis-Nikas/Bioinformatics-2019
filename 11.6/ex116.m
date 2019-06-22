e9af7018066c3349411ce4bd2f6490cc3f9081b2
Unknown <p16097@students.cs.unipi.gr>
2019-06-23 00:02:57 +0300

    vazo to 11.6

--- 11.6/ex116.m
+++ 11.6/ex116.m
@@ -2,15 +2,16 @@ clear
 clc
 A = [0.9 0.1; 0.1 0.9];
 B = [0.4 0.1 0.4 0.1; 0.2 0.3 0.2 0.3;]';
-katastaseis = ['α','β'] 
+katastaseis = ['a','b'] ;
 
 x = nt2int('GGCT');
 prob_init = [0.5; 0.5];
-seq = [x]
+seq = [x];
 [score,best_path] = viterbi(prob_init,A,B,x);
 best_path = fliplr(best_path);
-disp("Best score was: ",score)
-for i=1:size(best_path),
-    best_path(i) = katastaseis(best
+disp("Best score was: "+score)
+final = '';
+for i=1:4,
+    final = [final  katastaseis(best_path(i))];
 end
-disp("Best path was: ",score)
+disp("Best path was: "+final)
--- 11.6/viterbi.m
+++ 11.6/viterbi.m
@@ -4,34 +4,35 @@ function [score,best_path] = viterbi(prob_init,A,B,x)
     M = size(B,1);
     T = length(x);
     C = zeros(N,T);
-    pred = size(N,T);
+    pred = size(T);
     
-    prob_init = log2(prob_init)
-    A = log2(A)
-    B = log2(B)
+    prob_init = log2(prob_init);
+    A = log2(A);
+    B = log2(B);
     for i=1:N
         C(i,1) = prob_init(i) + B(x(1),i);
-        [C(i,t),index] = max(v);
-         pred(i,t) = index;
     end
+    [score,pred(1)] = max(C(:,1));
     for t = 2:T
         for i = 1:N
             v = zeros(1,N);
             for p = 1:N
                 v(p)= C(p,t-1)+A(p,i)+B(x(t),i);
             end
-            [C(i,t),index] = max(v);
-            pred(i,t) = index;
+            [C(i,t),index] = max(v); 
         end
+        [score,pred(t)] = max(C(:,t));
     end
+    
+    
+    
     
     %viterbi score
     [score,index] = max((C(:,T)));
     t = T;
     best_path = [];
-    while pred(index,t)~=0,
-        best_path = [best_path int2nt(index)];
-        index = pred(index,t);
+    while t~=0,
+        index = pred(t);
+        best_path = [best_path index];
         t = t-1;
-        
     end
