00fe668c4f56cc053d11df0b558f46eb5a2d2d9f
Unknown <p16097@students.cs.unipi.gr>
2019-06-29 14:16:02 +0300

    resolve

--- 6.14/ex614.m
+++ 6.14/ex614.m
@@ -3,7 +3,13 @@ clc
 options = ["brain", "liver", "muscle"];
 prompt = 'Please select two of the available nucleotides: brain, liver and muscle \n Type 1,2 or 3 accordingly with brackets (ex. [1 2])\n'; 
 type = [input(prompt)];
+<<<<<<< Updated upstream
 while (type <= 0) | ((type > 3) | (~length(type) == 2))%%if type smaller than 0 or type bigger than 3 or input was 3 numbers
+=======
+%%if type smaller than 0 or type bigger than 3 or input was 3 numbers
+while (~(length(type) == 2)), 
+    
+>>>>>>> Stashed changes
  prompt = 'Please select two of the available nucleotides: brain, liver and muscle\n Type 1,2 or 3 accordingly with coma (ex. [1 2])\n \n'; 
  type = [input(prompt)];
 end
@@ -17,7 +23,7 @@ win_combos = []
 
 for i=0:length(seq1),
     for j = 0:length(seq2),
-        if((i-1 >= 0) && (j-2 >= 0)) | ((i-2 >= 0) && (j-1 >= 0)), %we can remove elements from both sequences as desribed
+        if((i-1 >= 0) && (j-2 >= 0)) || ((i-2 >= 0) && (j-1 >= 0)), %we can remove elements from both sequences as desribed
             
         else, %player won
             win_combos = [win_combos; [i j]]
